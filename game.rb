bddc560b1a1903ebf13a06ab9978e0f23e8ab84c
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-17 13:58:40 -0400

    create game class

---
+++ game.rb
@@ -0,0 +1,55 @@
+# Game: 
+#   role: keep track of which player goes and takes in input
+#   needs: 
+#     player names (2)
+#     keeps track of input/output
+#     method: run game (loop to alternate between players)
+#       have conditionals in loop 
+
+class Game
+
+  attr_accessor 
+
+  def initialize()
+    @game_over = false
+    @players = []
+    @players[0] = Player.new('Player 1')
+    @players[1] = Player.new('Player 2')
+    @turn = 0
+  end
+
+  def start 
+    while !@game_over do #while game isn't over, do all this stuff
+      current_player = @players[@turn % 2] #is turn divisible by 2? play only on even number turns
+      other_player = @players[(@turn + 1) % 2] #only goes on odd numbers
+      new_question = Question.new(current_player.name)
+      new_question.random_question #will ask question 
+      input = gets.chomp.to_i #converting input to integer
+      if new_question.validate(input)
+        puts "CORRECT!"
+      else 
+        puts "INCORRECT"
+        current_player.live_change
+        if current_player.lives == 0 
+          puts "#{other_player.name} WINS WITH A SCORE OF #{other_player.lives}/3"
+          @game_over = true
+          break
+        end 
+      end 
+    puts "P1 #{@players[0].lives}, P2 #{@players[1].lives}" 
+    @turn = @turn + 1
+  end 
+
+end
+
+# ------NEW TURN-------
+# P1, P2 lives 
+# CORRECT!
+# INCORRECT! 
+# Player 1 wins with a score of score 
+# Good bye!
+
+
+# p = Player.new('L. Ron')
+# puts p.name 
+# puts p.lives
