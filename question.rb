217858b8db28fb6d2ab938e6b811e061eb2642fb
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-17 13:44:42 -0400

    add player as initializing val bc questions are per player

--- question.rb
+++ question.rb
@@ -2,14 +2,15 @@ class Question
   
   attr_reader :answer
 
-  def initialize
+  def initialize(player)
+    @player = player
     @value1 = rand(1..20)
     @value2 = rand(1..20)
     @answer = @value1 + @value2
   end
 
   def random_question
-    puts "What is: #{@value1} plus #{@value2}"
+    puts "#{@player} What is: #{@value1} plus #{@value2}"
   end 
 
   def validate(input)
