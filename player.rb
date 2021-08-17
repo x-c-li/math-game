e3478a7a00bd14a61c401ed4c908e8cc8244c603
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-17 13:42:59 -0400

    create player, add in names and lives

---
+++ player.rb
@@ -0,0 +1,18 @@
+class Player
+
+  attr_accessor :name :lives#reads and writes values
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def live_change 
+    @lives = @lives - 1
+  end 
+
+end
+
+p = Player.new('L. Ron')
+puts p.name 
+puts p.lives
